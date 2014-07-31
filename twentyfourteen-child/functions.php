d7e9071b2d936ef0f7c0b1f2df8c5b433da27b89
Toby Cryns <toby@themightymo.com>
2014-07-30 23:14:50 -0500

    Removed erroneous jquery call in functions.php

--- twentyfourteen-child/functions.php
+++ twentyfourteen-child/functions.php
@@ -624,7 +624,6 @@ function pluginname_ajaxurl() {
 ?>
 
 <link rel="stylesheet" href="//code.jquery.com/ui/1.10.4/themes/smoothness/jquery-ui.css">
-<script src="//code.jquery.com/jquery-1.10.2.js"></script>
 <script src="//code.jquery.com/ui/1.10.4/jquery-ui.js"></script>
 
 <style>
