4d468dd1010f49cb2a3f2bfe37cd61da47dbc142
Toby Cryns <toby@themightymo.com>
2014-08-12 14:48:56 -0500

    Moved Live Edit call to the header div

--- twentyfourteen-child/content-userstory.php
+++ twentyfourteen-child/content-userstory.php
@@ -10,10 +10,10 @@
  */
 ?>
 
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?><?php if(function_exists('live_edit')) { live_edit('post_title, post_content, how_many_hours_will_this_to-do_require, assigned_to'); }?>>
+<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 	<?php twentyfourteen_post_thumbnail(); ?>
 
-	<header class="entry-header">
+	<header class="entry-header" <?php if(function_exists('live_edit')) { live_edit('post_title, post_content, how_many_hours_will_this_to-do_require, assigned_to'); }?>>
 		<?php if ( in_array( 'category', get_object_taxonomies( get_post_type() ) ) && twentyfourteen_categorized_blog() ) : ?>
 		<div class="entry-meta">
 			<span class="cat-links"><?php echo get_the_category_list( _x( ', ', 'Used between list items, there is a space after the comma.', 'twentyfourteen' ) ); ?></span>
