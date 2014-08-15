0e6672b2fbcd0f37a9714080552fe3475332a176
Toby Cryns <toby@themightymo.com>
2014-08-14 21:44:23 -0500

    Added total to hours worked column in All Hours Worked view

--- tmm-datatables/js/custom.js
+++ tmm-datatables/js/custom.js
@@ -1,4 +1,31 @@
 jQuery(document).ready(function($) {
-    //alert('test');
-    $('#myTable').DataTable();
+    var table = $('#myTable').DataTable( {
+    	//via http://www.datatables.net/examples/advanced_init/footer_callback.html
+        "footerCallback": function ( row, data, start, end, display ) {
+            var api = this.api(), data;
+ 
+            // Remove the formatting to get integer data for summation
+            var intVal = function ( i ) {
+                return typeof i === 'string' ?
+                    i.replace(/[\$,]/g, '')*1 :
+                    typeof i === 'number' ?
+                        i : 0;
+            };
+ 
+            
+ 
+            // Total over this page
+            data = api.column( 1, { page: 'current'} ).data();
+            pageTotal = data.length ?
+                data.reduce( function (a, b) {
+                        return intVal(a) + intVal(b);
+                } ) :
+                0;
+ 
+            // Update footer
+            $( api.column( 1 ).footer() ).html(
+                'Hours Displayed Above: '+pageTotal
+            );
+        }
+    } );	
 });
