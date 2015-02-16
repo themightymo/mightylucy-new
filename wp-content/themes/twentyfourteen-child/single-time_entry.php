4c7cee3a13b63896954a1eea81a0fbd2e2cd0bd3
Toby Cryns <toby@themightymo.com>
2015-02-16 09:50:24 -0600

    Added time entry to single time_entry front-end view

---
+++ wp-content/themes/twentyfourteen-child/single-time_entry.php
@@ -0,0 +1,87 @@
+<?php
+/**
+ * The Template for displaying all single posts
+ *
+ * @package WordPress
+ * @subpackage Twenty_Fourteen
+ * @since Twenty Fourteen 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="content-area">
+		<div id="content" class="site-content" role="main">
+			<?php
+				// Start the Loop.
+				while ( have_posts() ) : the_post();
+
+					/*
+					 * Include the post format-specific template for the content. If you want to
+					 * use this in a child theme, then include a file called called content-___.php
+					 * (where ___ is the post format) and that will be used instead.
+					 */
+					get_template_part( 'content', 'userstory' );
+
+					// Previous/next post navigation.
+					//twentyfourteen_post_nav();
+
+					// If comments are open or we have at least one comment, load up the comment template.
+					if ( comments_open() || get_comments_number() ) {
+						comments_template();
+					}
+					
+					$arr_user_story_done_or_not=get_field('user_story_done_or_not');
+					$doneornot=get_term($arr_user_story_done_or_not[0],'user_story_done_or_not');
+					$options='<tr><td><h5>Status:</h5></td><td><select id="bottomselect" status="'.$doneornot->slug.'xxx'.get_the_ID().'" class="user_story_status"> <option  value="activexxx'.get_the_ID().'"> Active</option><option  value="donexxx'.get_the_ID().'"> Done</option> <option  value="on-holdxxx'.get_the_ID().'"> On Hold</option><option  value="ready-for-client-reviewxxx'.get_the_ID().'" > Ready For Client Review</option></select></td></tr>';
+					
+					?>
+					
+					<div id="b_story_status"><table><?php echo $options;?></table><div class="ajax_status"></div></div>
+				<?php endwhile;
+			?>
+			
+			<div class="entry-content">
+			
+				<?php 
+				if ( current_user_can('manage_options') ) { ?>
+					This to-do required <?php echo get_field('hours_invested'); ?> hours. <?php
+				} ?>
+			
+			</div><!-- .entry-content -->
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php
+get_sidebar( 'content' );
+get_sidebar();
+
+?>
+<script>
+(function($) {
+    $(".user_story_status").val($(".user_story_status").attr('status'));
+      
+   $(document).on("change",".user_story_status",function(){
+       var data = {
+            action: 'change_status',
+            data_status:$(this).val(), 
+        };
+		this_value=$(this).val();
+
+        // since 2.8 ajaxurl is always defined in the admin header and points to admin-ajax.php
+        $.post(ajaxurl, data, function(response) {
+		    //alert(response);
+           $('.ajax_status').html(response);
+		   
+		   $("#topselect").attr(this_value);
+		   $("#bottomselect").attr(this_value);
+		   $("#topselect").val(this_value);
+		   $("#bottomselect").val(this_value);
+        }); 
+	});
+	
+})( jQuery );
+</script>
+<?php
+
+get_footer();
