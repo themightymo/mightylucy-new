79e982e75b3eec968c8a3b06780505542b54b7a9
abelboy <abel@themightymo.com>
2015-02-17 23:25:35 +0800

    code clean, add checks to add_action() with is_admin, removed the readme file for wordpress.org

--- wp-content/plugins/tmm-display-posts-by-recency/tmm-display-posts-by-recency.php
+++ wp-content/plugins/tmm-display-posts-by-recency/tmm-display-posts-by-recency.php
@@ -17,21 +17,22 @@ function custom_post_order($query) {
 		_builtin => true returns WordPress default post types. 
 		_builtin => false returns custom registered post types. 
 	*/
-//	$post_types = get_post_types(array('_builtin' => true), 'names');
 	$post_types = array( 'user_story', 'time_entry', 'post' );
+
 	/* The current post type. */
 	$post_type = $query->get('post_type');
+
 	/* Check post types. */
 	if(in_array($post_type, $post_types)) {
-		/* Post Column: e.g. title */
 		if( $query->get( 'orderby' ) == '') {
 			$query->set( 'orderby', 'date' );
  		}
- 		/* Post Order: ASC / DESC */
  		if($query->get('order') == '') {
 	 		$query->set('order', 'DESC');
 	 	}
 	}
 }
 
-add_action('pre_get_posts', 'custom_post_order');
+if ( is_admin() ) {
+	add_action('pre_get_posts', 'custom_post_order');
+}
