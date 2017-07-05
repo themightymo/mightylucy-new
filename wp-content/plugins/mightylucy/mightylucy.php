5fb864c1f1c70a80768468a9894e903e34ad14a3
Toby Cryns <toby@themightymo.com>
2017-07-05 18:48:45 -0500

    added todo shortcode

--- wp-content/plugins/mightylucy/mightylucy.php
+++ wp-content/plugins/mightylucy/mightylucy.php
@@ -3,7 +3,7 @@
 Plugin Name: MightyLucy
 Plugin URI: https://www.themightymo.com/
 Description: Adds functionality to mightylucy website.  
-Author: themightymo
+Author: themightymo, Toby Cryns
 Author URI: https://www.themightymo.com
 License: GPLv2 or later
 Text Domain: themightymo
@@ -24,7 +24,7 @@ You should have received a copy of the GNU General Public License
 along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 
-Copyright 2016 The Mighty Mo! Design Co.
+Copyright 2017 The Mighty Mo! Design Co. LLC
 */
 
 // If post type is time_entry, then display the date worked	
@@ -54,3 +54,48 @@ function return_acf_time_entry_content ( $content ) {
 
     return $content;
 }
+
+// Shortcode: To-Dos List
+function todos_shortcode( $atts ) {
+
+	// Attributes
+	$atts = shortcode_atts(
+		array(
+			'todo_customer' => 'null',
+			'todo_assigned_to' => 'null',
+		),
+		$atts,
+		'todos'
+	);
+	
+	function query_todos() {
+		// WP_Query arguments
+		$args = array(
+			'post_type'=> array( 'todo' ),
+		);
+		
+		// The Query
+		$query = new WP_Query( $args );
+		
+		// The Loop
+		if ( $query->have_posts() ) {
+			while ( $query->have_posts() ) {
+				$query->the_post();
+				
+				echo '<a href="' . get_permalink() . '">' . get_the_title() . '</a><br>';
+				
+				//var_dump($query);
+			}
+		} else {
+			// no posts found
+		}
+				
+		// Restore original Post Data
+		wp_reset_postdata();
+	    
+	}
+	
+	query_todos();
+
+}
+add_shortcode( 'todos', 'todos_shortcode' );
